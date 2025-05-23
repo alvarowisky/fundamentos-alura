859eb7bfcde9c1bc594c11e409f11547705e4310
Wisky | Fisioterapia, Psicologia e Tecnologia <149326882+alvarowisky@users.noreply.github.com>
2025-05-23 08:23:46 -0300

    Add files via upload

---
+++ atualizar-lista.js
@@ -0,0 +1,5 @@
+const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+
+listaEstudantes.splice(1, 2, 'Rodrigo');
+
+console.log(listaEstudantes); // ['João', 'Rodrigo', 'Fernanda', 'Gustavo', 'Juliana', 'Leo']
---
+++ juntar-salas.js
@@ -0,0 +1,6 @@
+const salaJS = ['Evaldo', 'Camis', 'Mari'];
+const salaPython = ['Ju', 'Leo', 'Raquel'];
+
+const salaUnificada = salaJS.concat(salaPython); // Unindo as duas salas
+
+console.log(salaUnificada); // ['Evaldo', 'Camis', 'Mari', 'Ju', 'Leo', 'Raquel']
